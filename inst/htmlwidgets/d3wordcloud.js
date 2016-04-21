598afc50a0a26b29008c478a51bc9fa606ba92bc
Matt Johnson <mrjoh3@gmail.com>
2016-04-21 12:22:27 +1000

    use provided color instead of scale
    Hi Joshua, 
    I was having trouble with the colors whenever I provided a color for each word if the word freq was the same for two or more words. It traced back to the behavior of d3.scale.ordinal() line 78. The change I have made is just to check for every_word_has_own_color and use the d.color if it is present. 
    
    This solves my problem but does not necessarily behave in the way you want it to but please have a look. 
    
    Matt

--- inst/htmlwidgets/d3wordcloud.js
+++ inst/htmlwidgets/d3wordcloud.js
@@ -137,7 +137,14 @@ HTMLWidgets.widget({
         .style("font-size", function(d) { return d.size + "px"; });
 
       text.style("font-family", x.pars.font)
-        .style("fill", function(d) { return colorscale(d.size); })
+        .style("fill", function(d) {
+           if (x.pars.every_word_has_own_color) {
+             return d.color;
+           } else {
+           return colorscale(d.size);
+           }
+           })
+
         .attr("data-toggle", "tooltip")
         .text(function(d) { return d.text; });
 
